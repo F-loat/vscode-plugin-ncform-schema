32348d6ccf9079f627621e854564eefb63a47b74
F-loat <chaimaoyuan@foxmail.com>
2020-06-23 22:08:06 +0800

    chore: remove unused dependency

--- src/extension/ncformSchemaEditor.ts
+++ src/extension/ncformSchemaEditor.ts
@@ -104,7 +104,7 @@ export class NcFormSchemaEditorProvider implements vscode.CustomTextEditorProvid
           <meta charset=utf-8>
           <meta http-equiv=X-UA-Compatible content="IE=edge">
           <meta name=viewport content="width=device-width,initial-scale=1">
-          <title>ncfrom schema editor</title>
+          <title>ncform schema editor</title>
           <link href=vscode://js/app.js rel=preload as=script>
           <link href=vscode://js/chunk-vendors.js rel=preload as=script>
           <style>
